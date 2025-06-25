9cb6a6bd76b2de76abdfb870c1d83d47557cb2bd
ricardoosorioc <42077272+ricardoosorioc@users.noreply.github.com>
2025-06-24 21:15:47 -0500

    Update LoginTest.scala

--- src/test/scala/Demo/LoginTest.scala
+++ src/test/scala/Demo/LoginTest.scala
@@ -16,7 +16,7 @@ class LoginTest extends Simulation{
   val scn = scenario("Login").
     exec(http("login")
       .post(s"users/login")
-      .body(StringBody("""{"email": "${email}", "password": "${password}"}""")).asJson
+      .body(StringBody(s"""{"email": "$email", "password": "$password"}""")).asJson
        //Recibir información de la cuenta
       .check(status.is(200))
       .check(jsonPath("$.token").saveAs("authToken"))
