f4014b19aa411a2e961b9d513f8122c566fcb378
ricardoosorioc <42077272+ricardoosorioc@users.noreply.github.com>
2025-06-26 18:18:12 -0500

    Update LoginTest.scala

--- src/test/scala/Demo/LoginTest.scala
+++ src/test/scala/Demo/LoginTest.scala
@@ -10,8 +10,7 @@ class LoginTest extends Simulation{
   // 1 Http Conf
   val httpConf = http.baseUrl(url)
     .acceptHeader("application/json")
-    //Verificar de forma general para todas las solicitudes.
-    .check(status.is(200))
+    //Verificar de forma general para todas las solicitudes.    
 
   // 2 Scenario Definition
   val scn = scenario("Login").
@@ -27,12 +26,13 @@ class LoginTest extends Simulation{
       http("Create Contact")
         .post(s"contacts")
         .header("Authorization", "Bearer ${authToken}")
-        .body(StringBody(s"""{"firstName": "Christian","lastName": "Gomez","birthdate": "1970-01-01","email": "jdoe@fake.com","phone": "8005555555","street1": "1 Main St.","street2": "Apartment A","city": "Anytown","stateProvince": "KS","postalCode": "12345","country": "USA"}""")).asJson
+        .body(StringBody(s"""{"firstName": "Ok","lastName": "Gomez","birthdate": "1970-01-01","email": "jdoe@fake.com","phone": "8005555555","street1": "1 Main St.","street2": "Apartment A","city": "Anytown","stateProvince": "KS","postalCode": "12345","country": "USA"}""")).asJson
         .check(status.is(201))
     )
 
+
   // 3 Load Scenario
   setUp(
-    scn.inject(rampUsersPerSec(5).to(15).during(30))
+    scn.inject(rampUsers(10).during(50))
   ).protocols(httpConf);
 }
